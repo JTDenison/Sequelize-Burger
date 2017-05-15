1fc41136110517f45ad800bb7f4213e1e70bf052
JTDenison <joshd_84@ymail.com>
2017-05-15 18:22:01 -0400

    heroku commit

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,25 @@
 {
     "development": {
+        "username": "root",
+        "password": "root",
+        "database": "burgers_db",
+        "host": "127.0.0.1",
+        "dialect": "mysql",
+        "port": "3336"
+    },
+    "test": {
+        "username": "root",
+        "password": "root",
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
         "username": "w3nw8mt2h1mmophe",
         "password": "ab2sjyeymenoqomp",
         "database": "burgers_db",
         "host": "s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         "dialect": "mysql",
         "port": "3306"
-    },
-    "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
-    },
-    "production": {
-        "use_env_variable": "JAWSDB_URL",
-        "dialect": "mysql"
     }
 }
-
-
-
-
-
-/* Host s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com   
-Username    w3nw8mt2h1mmophe    
-Password    ab2sjyeymenoqomp    
-Reset
-Port    3306
-
-*/
-
